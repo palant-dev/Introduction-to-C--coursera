f34ec37a262ef127960fc9d59ecc8bc6cdda90b2
apalomba22 <113528707+apalomba22@users.noreply.github.com>
2023-03-22 17:53:05 +0100

    solving the issue with initial commit

---
+++ my-first-code/my-first-code/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace my_first_code;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, my name is Antonio");
+        Console.WriteLine("My friend name is Geppetto");
+    }
+}
+
